c7a79593cac5bbd92ef59fb4d1efd06039a5d1d3
smw4 <7003183+smw4@users.noreply.github.com>
2022-09-14 18:21:47 -0400

    steps 1-6 working. Attempting to implement the addPlayer() method to the team object

--- TeamStats.js
+++ TeamStats.js
@@ -9,17 +9,29 @@ const team = {
 		{ firstName: 'Ron', lastName: 'Groen', age: 30 }, //player 3
 	],
 	_games: [
-        { opponent: "Beagles", teamPoint: 42, opponentPoints: 45 },
-        { opponent: "Beagles", teamPoint: 32, opponentPoints: 95 },
-        { opponent: "Beagles", teamPoint: 24, opponentPoints: 5 },
-    ],
+		{ opponent: 'Beagles', teamPoint: 42, opponentPoints: 45 },
+		{ opponent: 'Beagles', teamPoint: 32, opponentPoints: 95 },
+		{ opponent: 'Beagles', teamPoint: 24, opponentPoints: 5 },
+	],
+	get players() {
+		// STEP 5.
+		return this._players;
+	},
+	get games() {
+		// STEP 6.
+		return this._games;
+	},
+	//STEP 7. NOT WORKING YET. Not throwing any error codes, but not appending "Sal Haroldino, 94 to the players list." Trying to let the computer know that the inputs for addPlayer should equate to the keys in _players
+	addPlayer(newFirstName, newLastName, newAge) {
+		newFirstName = this._players.firstName;
+		newLastName = this._players.lastName;
+		newAge = this._players.age;
+		//if newFirstName, newLastName === strings AND newAge === 'number'
+		return this._players.push(); //use the .push() method to add object to array
+	},
 };
+
+team.addPlayer('Sal', 'Haroldino', 94);
 
 console.log(team._players);
 console.log(team._games);
-
-
-
-get players() { // STEP 5. Not sure this is correct!
-    return _players;
-}
